d4167be517705aab45f6df61c2222d80a490a1cd
Giulia844 <giulia.pisi3@studio.unibo.it>
2025-05-23 11:36:18 +0200

    Add files via upload

--- gameObjects/Player.java
+++ gameObjects/Player.java
@@ -9,49 +9,62 @@ import java.awt.*;
 
 public class Player extends Controllable {
 
+    private boolean isWinner;
+    private int life;
+    private Coordinates initialCoordinates;
+
+// Costruttore: inizializza le coordinate e lo stato del giocatore
     public Player(Coordinates coordinates) {
         super(coordinates);
+        this.initialCoordinates = new Coordinates(coordinates.getX(), coordinates.getY());
+        this.life = 3; // vite iniziali
+        this.isWinner = false;
     }
 
     @Override
     public void move(Direction direction) {
-        //TODO IMPLEMENT
+        // Aggiorna la direzione del movimento
+        setDirection(direction);
     }
 
     @Override
     public void update() {
-        //TODO IMPLEMENT
+        // Aggiorna la posizione del giocatore in base alla direzione
+        getCoordinates().moveTo(getCoordinates().next(getDirection()));
     }
 
     public boolean isWinner() {
-        //TODO IMPLEMENT
-        return false;
+        return isWinner;
     }
 
     public boolean isLooser() {
-        //TODO IMPLEMENT
-        return false;
+        // Il giocatore ha perso se ha finito le vite
+        return life <= 0;
     }
 
     public void markAsWinner() {
-        //TODO IMPLEMENT
+        isWinner = true;
     }
 
     public void handleDamage() {
-        //TODO IMPLEMENT
+        // Riduce le vite di 1 (ma non va sotto zero)
+        if (life > 0) {
+            life--;
+        }
     }
 
     @Override
     public DrawingInformation draw() {
-        return new DrawingInformation(' ', Color.BLACK);
+        // Disegna Pac-Man come un cerchio giallo (es. 'C')
+        return new DrawingInformation('C', Color.YELLOW);
     }
 
     public void resetCoordinates() {
-        //TODO IMPLEMENT
+        // Riporta il giocatore alla posizione iniziale
+        getCoordinates().moveTo(initialCoordinates);
     }
 
     public int getLife() {
-        //TODO IMPLEMENT
-        return 0;
+        return life;
     }
 }
