bb9d8a3ef8b7eae9f79f728ba1c872f28e5927c2
Alice-003 <alice.minuzzo@studio.unibo.it>
2025-05-26 10:42:25 +0200

    Add files via upload

--- gameObjects/Food.java
+++ gameObjects/Food.java
@@ -7,28 +7,34 @@ import utils.DrawingInformation;
 import java.awt.*;
 
 public class Food extends Drawable {
+    private boolean cancella;
 
     public Food(Coordinates coords) {
         super(coords);
+
         //TODO: IMPLEMENT
+        this.cancella = false; //Flag per indicare se il cibo è stato mangiato o meno (inizialmente no)
     }
 
     @Override
-    public void update() {}
+    public void update() {} //Non necessario per via dell'implementazione che già è presente
 
     @Override
     public DrawingInformation draw() {
         //TODO: IMPLEMENT
-        return new DrawingInformation(' ', Color.BLACK);
+        if(cancella) return new DrawingInformation(' ', Color.BLACK) ; //Cibo mangiato
+        else return new DrawingInformation('f', Color.YELLOW) ; //Cibo non mangiato
     }
 
-    public void markAsDeleted() {
+    public void markAsDeleted() { //Segna se il cibo è stato mangiato
         //TODO: IMPLEMENT
+        this.cancella = true; //Il giocatore ha fatto una collisione con food
     }
 
-    public boolean isDeleted() {
+    public boolean isDeleted() { //Verifica se il cibo è stato mangiato
         //TODO: IMPLEMENT
-        return false;
+        //return false
+        return this.cancella; //Restituisce il valore effettivo di "cancella" che dovrebbe essere false, sennò si andrebbe in markAsDeleted()
     }
 
 }
--- gameObjects/Player.java
+++ gameObjects/Player.java
@@ -9,49 +9,94 @@ import java.awt.*;
 
 public class Player extends Controllable {
 
+    private boolean isWinner;
+    private int life;
+    private Coordinates initialCoordinates;
+    private Direction direction;
+
+// Costruttore: inizializza le coordinate e lo stato del giocatore
     public Player(Coordinates coordinates) {
         super(coordinates);
+        this.coordinates = coordinates;
+        this.initialCoordinates = new Coordinates(coordinates.getRow(), coordinates.getCol());
+        this.life = 3; // vite iniziali
+        this.isWinner = false;
+        this.direction = Direction.UP;
+    }
+    
+    // Metodo per aggiornare la direzione
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 
+    // Metodo per leggere la direzione corrente
+    public Direction getDirection() {
+        return this.direction;
+    }
+    
+  //Metodo privato che calcola nuove coordinate in base alla direzione data
+    private Coordinates getNextCoordinates(Direction direction) {
+        int row = coordinates.getRow();
+        int col = coordinates.getCol();
+
+        switch (direction) {
+            case UP:    row -= 1; break;
+            case DOWN:  row += 1; break;
+            case LEFT:  col -= 1; break;
+            case RIGHT: col += 1; break;
+            case STAY:  // non si muove
+                break;
+        }
+        // Ritorna un nuovo oggetto Coordinates con la nuova posizione
+        return new Coordinates(row, col);
+    }
+    
     @Override
     public void move(Direction direction) {
-        //TODO IMPLEMENT
+        // Aggiorna la direzione del movimento
+        setDirection(direction);
     }
 
     @Override
     public void update() {
-        //TODO IMPLEMENT
+        // Aggiorna la posizione del giocatore in base alla direzione
+        Coordinates newCoords = getNextCoordinates(direction); 
+        this.coordinates = newCoords;
     }
 
     public boolean isWinner() {
-        //TODO IMPLEMENT
-        return false;
+        return isWinner;
     }
 
     public boolean isLooser() {
-        //TODO IMPLEMENT
-        return false;
+        // Il giocatore ha perso se ha finito le vite
+        return life <= 0;
     }
 
     public void markAsWinner() {
-        //TODO IMPLEMENT
+        isWinner = true;
     }
 
     public void handleDamage() {
-        //TODO IMPLEMENT
+        // Riduce le vite di 1 (ma non va sotto zero)
+        if (life > 0) {
+            life--;
+        }
     }
 
     @Override
     public DrawingInformation draw() {
-        return new DrawingInformation(' ', Color.BLACK);
+        // Disegna Pac-Man come un cerchio giallo 
+        return new DrawingInformation('P', Color.YELLOW);
     }
 
     public void resetCoordinates() {
-        //TODO IMPLEMENT
+        // Riporta il giocatore alla posizione iniziale
+        this.coordinates = new Coordinates (initialCoordinates.getRow(), initialCoordinates.getCol());
+       
     }
 
     public int getLife() {
-        //TODO IMPLEMENT
-        return 0;
+        return life;
     }
 }
